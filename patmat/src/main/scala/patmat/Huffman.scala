bc6867026b0fb83009b1e213c5587e359d3d5930
Sebas LG <neoranga55@gmail.com>
2016-06-18 19:04:00 +0200

    Convert Huffman tree to lookup table w4.5.1

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -274,14 +274,27 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-    def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = {
+    def convertTree(tree: CodeTree, accu: List[Bit]): CodeTable = {
+      tree match {
+        case Leaf(char, _) => List[(Char, List[Bit])]((char, accu))
+        case Fork(left, right, _, _) => {
+          mergeCodeTables(convertTree(left, accu ::: List[Bit](0)),
+            convertTree(right, accu ::: List[Bit](1)))
+        }
+      }
+    }
+    convertTree(tree, List[Bit]())
+  }
   
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-    def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = {
+    a ::: b
+  }
   
   /**
    * This function encodes `text` according to the code tree `tree`.
